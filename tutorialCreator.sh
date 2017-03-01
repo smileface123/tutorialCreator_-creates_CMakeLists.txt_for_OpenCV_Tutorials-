3bbd07e03bfba45201861caadf44417e2eff9801
Valentin Schild <valentinschild@gmail.com>
2017-03-01 16:40:46 +0100

    added comments

--- tutorialCreator.sh
+++ tutorialCreator.sh
@@ -10,13 +10,18 @@ echo *.cpp files for you.
 echo Please specify the name of your new Project.
 echo Press Ctr + C to abort
 
+# Read user input.
 read PROJECT
 
+# Create directory.
 mkdir $PROJECT
+# Change to directory.
 cd $PROJECT
 
+# Create CMakeLists.txt.
 touch CMakeLists.txt
 
+# Write data to CMakeLists.txt.
 echo "cmake_minimum_required(VERSION 2.8)" >> CMakeLists.txt
 echo "project( $PROJECT )" >> CMakeLists.txt
 echo "find_package( OpenCV REQUIRED )" >> CMakeLists.txt
@@ -24,4 +29,5 @@ echo "include_directories( \${OpenCV_INCLUDE_DIRS} )" >> CMakeLists.txt
 echo "add_executable( $PROJECT $PROJECT.cpp )" >> CMakeLists.txt
 echo "target_link_libraries( $PROJECT \${OpenCV_LIBS} )" >> CMakeLists.txt
 
+# Create cpp file.
 touch $PROJECT.cpp
